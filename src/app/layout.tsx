1aaac1dcf6ba818c5f682e752a043d1d264e8045
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-07-19 21:56:00 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Blinker } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const font_google = Blinker({ weight: ['300', '400', '600'], style: ['normal'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font_google.className}>{children}</body>
     </html>
   )
 }
