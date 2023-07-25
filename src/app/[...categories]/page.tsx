f4575744a909d02af03d9c2727e3eb7220b53555
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-07-25 15:26:40 -0300

    add: Page Category

---
+++ src/app/[...categories]/page.tsx
@@ -0,0 +1,11 @@
+import { Breadcrumbs } from "@/components/Breadcrumbs/breadcrumbs";
+import { PageCategories } from "@/components/Pages/Categories/categories";
+
+export default function Categories(){
+    return(
+        <>
+        <Breadcrumbs />
+        <PageCategories />
+        </>
+    )
+}
