1b4dfe75aadfde148809e61e949d03b974d1bc7c
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-07-25 12:36:25 -0300

    fix: Products

--- src/components/Products/listProducts.tsx
+++ src/components/Products/listProducts.tsx
@@ -1,19 +1,19 @@
 import { TitleHome } from "@/components/Titles/titleHome";
 import ProductImg from '@/assets/img/product.jpg'
-import { OneProductList } from "./oneProductList";
+import { OneProduct } from "./oneProduct";
 
 interface ListProducts {
 
 }
 export function ListProducts(props : ListProducts){
     return(
-        <div className="my-24">
+        <div>
             <TitleHome title='Produtos em Destaque' />
 
             <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-3 sm:gap-4">
 
                 {Array.from({length: 8}, (_,index) =>(
-                    <OneProductList key={index} src={ProductImg} name="Poltrona de pele de carneiro" price="20,00" />
+                    <OneProduct key={index} src={ProductImg} name="Poltrona de pele de carneiro" price="20,00" />
                 ))}
 
                
--- src/components/Products/oneProduct.tsx
+++ src/components/Products/oneProduct.tsx
@@ -5,12 +5,12 @@ interface OneProductListProps {
     name: string;
     price: string;
 }
-export function OneProductList(props : OneProductListProps){
+export function OneProduct(props : OneProductListProps){
     return(
         <div className="cursor-pointer p-3 border-2 border-gray-200 hover:border-black transition-colors duration-300 ">
             <Image alt={`Product ${props.name}`} src={props.src} height={250} width={250} className="w-full" />
             <div>
-                <p className="text-lg">{props.name}</p>
+                <p className="text-lg whitespace-nowrap text-ellipsis overflow-hidden">{props.name}</p>
                 <p className=" text-2xl font-bold">R$ {props.price}</p>
             </div>
         </div> 
