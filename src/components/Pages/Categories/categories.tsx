55034e40fab0cdacc63980b01265691dc14c1db3
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-08-01 10:52:39 -0300

    fix: page category

--- src/components/Pages/Categories/categories.tsx
+++ src/components/Pages/Categories/categories.tsx
@@ -1,58 +1,52 @@
 import { ListProducts } from "@/components/Products/listProducts"
+import { Product } from "@/types/prodruct"
+import { getCategoryName, nameCategory } from "@/utils/utils";
 import Link from "next/link"
 
 interface PageCategoriesProps {
-
+    slug?: string | undefined;
+    dataProducts: Product[];
 }
 
-const ArrayCategory = [
-    {
-        slug: 'all',
-        name: 'Todos'
-    },
-    {
-        slug: 'furnitures',
-        name: 'Móveis'
-    },
-    {
-        slug: 'electronics',
-        name: 'Eletrônicos'
-    },
-    {
-        slug: 'lamps',
-        name: 'Lâmpadas'
-    },
-    {
-        slug: 'kitchen',
-        name: 'Cozinha'
-    },
-    {
-        slug: 'chairs',
-        name: 'Cadeiras'
-    },
-    {
-        slug: 'cosmetic',
-        name: 'Cosméticos'
-    }
-    
-]
 
-export function PageCategories(props : PageCategoriesProps){
+export function PageCategories({slug, dataProducts} : PageCategoriesProps){
+
+
+    let filteredProduct = dataProducts;
+    if(slug){
+        filteredProduct = dataProducts.filter(x => x.category === slug);
+    }
+
     return(
         <>
-             <h1 className="text-4xl font-bold uppercase mb-10 text-center">Categorias</h1>
+             <h1 className="text-4xl font-bold uppercase mb-10 text-center">{getCategoryName(slug)}</h1>
 
             <div className="flex flex-wrap gap-3 justify-center w-full mb-14">
-                {ArrayCategory.map(category => (
-                    <Link key={category.slug} href={`/categories/${category.slug}`}>
-                        <div className="bg-white px-4 text-sm sm:text-base py-1 border-2 border-gray-300 hover:border-gray-900 transition-colors duration-500">
-                            {category.name}
-                        </div>
-                    </Link>
-                ))}
-            </div>
+                {nameCategory.map((item, i) => {
+                    const selectMenu = slug == Object.keys(item)[0] ? 'border-black text-cl-primary' : '';
+                    const selectAll = !slug ? 'border-black text-cl-primary' : '';
+                    
+                    const categoryKey = Object.keys(item)[0];
+                    return categoryKey == '0' ? (
+                        <Link key={i} href='/categories/'>
+                            <div className={`bg-white px-4 text-sm sm:text-base py-1 border-2 border-gray-300 hover:border-gray-900 transition-colors duration-500 ${selectAll}`}>
+                                Todos
+                            </div>
+                        </Link>
+                    ):(
 
-            <ListProducts />
+                        <Link key={i} href={`/categories/${categoryKey}`}>
+                            <div className={`bg-white px-4 text-sm sm:text-base py-1 border-2 border-gray-300 hover:border-gray-900 transition-colors duration-500 ${selectMenu}`}>
+                                {Object.values(item)[0]}
+                            </div>
+                        </Link>
+                    );
+
+                    })}
+            </div>
+            
+            
+            <ListProducts dataProducts={filteredProduct} />
         </>
     )
 }
