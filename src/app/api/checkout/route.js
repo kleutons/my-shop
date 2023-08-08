4075a0b7ad47897ab938a7b2ece57ebcaf432f6f
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-08-08 16:38:12 -0300

    fix: router checkout

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,14 +1,31 @@
 import { NextResponse } from "next/server";
 import { Stripe } from "stripe";
 
-export async function POST(request) {
+
+export const POST = async (request) => {
+
+ try {
   const { dataItems } = await request.json();
-  
+
   const urlOrigin = request.headers.get('origin');
   const urlReferer = request.headers.get('referer');
+
+  if (!Array.isArray(dataItems)) {
+    return NextResponse.json(
+      { data: 'Invalid data: dataItems should be an array' },
+      { status: 400 }
+    );
+  }
+
+  for (const item of dataItems) {
+    if (!item.price || !item.quantity) {
+      return NextResponse.json(
+        { data: 'Invalid data: price and/or quantity missing in one or more items' },
+        { status: 483 }
+      );
+    }
+  }
   
-
-
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
   const session = await stripe.checkout.sessions.create({
@@ -19,7 +36,10 @@ export async function POST(request) {
     cancel_url: urlReferer,
   });
 
-  return NextResponse.json({
-    url: session.url
-  });
+  return NextResponse.json({ url: session.url });
+
+ } catch (error) {
+  console.error("An error occurred:", error);
+  return NextResponse.json({data: 'An error occurred while processing your request!'}, { status: 483 });
+}
 }
